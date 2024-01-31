63808a61c294b8e944d1abed5eebe00a0f1ef9d5
davis <davisdeloach@gmail.com>
2024-01-31 16:09:51 -0600

    commit

--- in-class-work/addingprogram.s
+++ in-class-work/addingprogram.s
@@ -1,20 +1,24 @@
 #a = b + c + d - e
-li x1, 10
-li x2, 10
-li x3, 10
-li x4, 10
-add x10, x1, x2
-add x10, x10, x3
-sub x10, x10, x4
+li x5, 10
+li x6, 10
+li x7, 10
+li x28, 10
+add x5, x5, x6
+add x5, x5, x7
+sub x5, x5, x28
 
 addi a0, x0, 1
 addi a1, x0, 42
 ecall
 
 #f = (g+h)-(i+j)
-add x5, x20, x21
-add x6, x22, x23
-sub x19, x5, x6
+li x5, 10
+li x6, 10
+li x7, 10
+li x28, 10
+add x5, x5, x6
+add x7, x7, x28
+sub x5, x5, x7
 
 addi   a0, x0, 1
 addi   a1, x0, 42
